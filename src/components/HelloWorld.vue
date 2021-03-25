349e6500bfce75d0cdb139b6bafea8423cec382a
DilipKheni10001 <discussionforall@gmail.com>
2021-03-25 09:27:34 +0530

    add slider code.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,55 @@
 <template>
- 
+<div>
+<h1>Hello</h1>
+  <swiper
+    :slides-per-view="3"
+    :space-between="50"
+    navigation
+    :pagination="{ clickable: true }"
+    :scrollbar="{ draggable: true }"
+    autoplay
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+  </swiper>
+</div>
 </template>
- 
 <script>
-export default {
-  name: 'HelloWorld',
-}
-  
-</script> 
+  // import Swiper core and required modules
+  import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/swiper.scss';
+  import 'swiper/components/navigation/navigation.scss';
+  import 'swiper/components/pagination/pagination.scss';
+  import 'swiper/components/scrollbar/scrollbar.scss';
+
+  // install Swiper modules
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+  // Import Swiper styles
+  export default {
+    name:"HelloWorld",
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    methods: {
+      onSwiper(swiper) {
+        console.log(swiper);
+      },
+      onSlideChange() {
+        console.log('slide change');
+      }
+    },
+  };
+</script>
